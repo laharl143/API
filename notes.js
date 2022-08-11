375069faca545b98bcb5e2986c90224bbb46ac26
Erskine Gamaliel L. Duenas <91664149+laharl143@users.noreply.github.com>
2022-08-11 12:08:47 +0800

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,19 @@
+////Notes for API////
+
+What is API? -Application Programming Interface
+
+//reference: https://home.openweathermap.org/users/sign_in
+
+https://api.openweathermap.org/data/2.5/weather?q=Manila&appid=e354898dddb8f14089e4162280f99989&units=metric   //added "&" + "units=metric"
+
+//bonus tip! you can use postman for cleaner API
+
+
+
+////JSON//////
+What is JSON? JavaScript Object Notation
+
+JSON is a format that is readable by humans
+
+
+//Helpful chrome extension: JSON viewer awesome
